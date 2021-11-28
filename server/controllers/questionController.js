f4a16363c68aaadfa678cb06eb0bed51caea448d
surangi1996 <55706550+surangi1996@users.noreply.github.com>
2021-11-28 16:30:40 +0530

    Question views by surangi

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -10,9 +10,9 @@ const Question = require('../models/Question');
 exports.question = async (req, res) => {
     try{
         const questions = await Question.find({});
-        res.render('question', { title: 'Social Network - Question',questions });
+        res.render('Question/question', { title: 'Social Network - Question',questions });
     }catch (error) {
-        res.satus(500).send({ message: error.message || "Error Occured" });
+        res.status(500).send({ message: error.message || "Error Occured" });
     }
 }
 
@@ -27,9 +27,9 @@ exports.submitQuestion = async (req, res) => {
     try{
         const infoErrorsObj = req.flash('infoErrors');
         const infoSubmitObj = req.flash('infoSubmit');
-        res.render('submit-question', { title: 'News Blog - Submit News',infoErrorsObj,infoSubmitObj });
+        res.render('Question/submit-question', { title: 'News Blog - Submit Question',infoErrorsObj,infoSubmitObj });
     }catch (error) {
-            res.satus(500).send({ message: error.message || "Error Occured" });
+            res.status(500).send({ message: error.message || "Error Occured" });
     }
 }
 
@@ -42,16 +42,17 @@ exports.submitQuestionOnPost = async (req, res) => {
         const newQuestion =new Question({
             question:req.body.question,
             answer:req.body.answer,
-            username:req.body.username
+            username:req.body.username,
+            field:req.body.field
         });
         await newQuestion.save();
         req.flash('infoSubmit','Question has been already added');
-        res.redirect('/question');
+        res.redirect('/Question/question');
         
     }catch (error) {
            // req.flash('infoErrors',error)
            res.json(error);
-            res.redirect('/submit-question');
+            res.redirect('/Question/submit-question');
     }
 }
 
@@ -65,9 +66,9 @@ exports.updateQuestion = async (req, res) => {
     try{
        let questionId = req.params.id;
        const getdetails = await Question.findById(questionId);
-       res.render('update-question', { title: 'News Blog - Submit News',getdetails });
+       res.render('Question/update-question', { title: 'News Blog - Update-Question',getdetails });
     }catch (error) {
-            res.satus(500).send({ message: error.message || "Error Occured" });
+            res.status(500).send({ message: error.message || "Error Occured" });
     }
 }
 
@@ -79,26 +80,18 @@ exports.updateQuestion = async (req, res) => {
 exports.updateQuestionRecord = async (req, res) => {
     try{
        let questionId = req.params.id;
-       const {question,answer}=req.body;
+       const {question,answer,field}=req.body;
        const updatingQuestion = {
            question,
-           answer
+           answer,
+           field
        }
        const update = await Question.findByIdAndUpdate(questionId,updatingQuestion);
-       res.redirect('/question');
+       res.redirect('/Question/question');
     }catch (error) {
-            res.satus(500).send({ message: error.message || "Error Occured" });
+            res.status(500).send({ message: error.message || "Error Occured" });
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -111,8 +104,8 @@ exports.deleteQuestion = async (req, res) => {
     try{
         let questionId = req.params.id;
         await Question.findByIdAndDelete(questionId);
-        res.redirect('/question');
+        res.redirect('/Question/question');
     }catch (error) {
-        res.satus(500).send({ message: error.message || "Error Occured" });
+        res.status(500).send({ message: error.message || "Error Occured" });
     }
 }
